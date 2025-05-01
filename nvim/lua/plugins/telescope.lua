77b967f734052c4a9e3f022d964c4bb015c5bfae
brl1 <bert.ROSELL@3ds.com>
2025-05-01 14:43:52 +0000

    pickers show filename first so it's easier to read

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -10,11 +10,34 @@ return {
       "nvim-tree/nvim-web-devicons"
     },
     config = function()
+      local function filenameFirst(_, path)
+        local tail = vim.fs.basename(path)
+        local parent = vim.fs.dirname(path)
+        if parent == "." then return tail end
+          return string.format("%s\t\t%s", tail, parent)
+        end
+
+        vim.api.nvim_create_autocmd("FileType", {
+          pattern = "TelescopeResults",
+          callback = function(ctx)
+            vim.api.nvim_buf_call(ctx.buf, function()
+            vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+            vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+          end)
+        end,
+      })
+
       require("telescope").setup({
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
           },
+        },
+        pickers = {
+          git_status = { path_display = filenameFirst, },
+          find_files = { path_display = filenameFirst, },
+          oldfiles = { path_display = filenameFirst, },
+          live_grep = { path_display = filenameFirst, },
         },
       })
       local builtin = require("telescope.builtin")
