f06f858b72aa7e69301332a47c1e9910ef07b33f
brl1 <bert.ROSELL@3ds.com>
2025-08-06 20:19:36 +0000

    some quality of life config improvements

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -7,6 +7,10 @@ vim.o.expandtab = true
 vim.opt.wrap = false
 
 vim.opt.cursorline = true
+vim.opt.colorcolumn = "100"                        -- Show column at 100 characters
+
+vim.opt.scrolloff = 10                             -- Keep 10 lines above/below cursor 
+vim.opt.sidescrolloff = 8                          -- Keep 8 columns left/right of cursor
 
 -- Enhanced cursorline highlight override after theme load
 vim.api.nvim_create_autocmd("ColorScheme", {
@@ -28,6 +32,10 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
+-- Split behavior
+vim.opt.splitbelow = true                          -- Horizontal splits go below
+vim.opt.splitright = true                          -- Vertical splits go right
+
 -- Set up line numbers
 vim.opt.relativenumber = true
 vim.opt.numberwidth = 3
@@ -35,3 +43,7 @@ vim.opt.numberwidth = 3
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
 
+-- Folding settings
+vim.opt.foldmethod = "expr"                             -- Use expression for folding
+-- vim.wo.vim.foldexpr = "v:lua.vim.treesitter.foldexpr()" -- Use treesitter for folding
+vim.opt.foldlevel = 99                                  -- Start with all folds open
