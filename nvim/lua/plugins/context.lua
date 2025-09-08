0965202e5297b0fa3c21b73d2d2622d62451757f
brl1 <bert.ROSELL@3ds.com>
2025-09-08 14:36:05 +0000

    fix tab stops and add context plugin

---
+++ nvim/lua/plugins/context.lua
@@ -0,0 +1,5 @@
+-- 'Pins' the file, class, method context to the top of the editor so you can see where you are in too large methods`
+return {
+  "nvim-treesitter/nvim-treesitter-context",
+  lazy = false,
+}
