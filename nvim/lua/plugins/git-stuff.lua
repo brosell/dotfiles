79ec828a774d4a1fd2350529b1f92ef307cf1ee8
brl1 <bert.ROSELL@3ds.com>
2025-09-30 23:14:16 +0000

    Remove lensline and add blamer

--- nvim/lua/plugins/git-stuff.lua
+++ nvim/lua/plugins/git-stuff.lua
@@ -12,24 +12,9 @@ return {
     end
   },
   {
-    'oribarilan/lensline.nvim',
-    -- https://github.com/oribarilan/lensline.nvim
-    -- tag = '2.0.0', -- or: 
-    branch = 'release/2.x',  -- for latest non-breaking updates
-    event = 'LspAttach',
+    "APZelos/blamer.nvim",
     config = function()
-      require('lensline').setup {
-        profiles = {
-          {
-            name = 'minimal',
-            style = {
-              placement = 'inline',
-              prefix = '',
-              render = "focused", -- optionally render lenses only for focused function
-            },
-          },
-        },
-      }
-    end,
-  }
+
+    end;
+  },
 }
