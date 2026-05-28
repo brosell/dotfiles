33799297f1b0998cb9b22a600fb5044992012a58
brl1 <bert.ROSELL@3ds.com>
2026-05-28 17:39:31 +0000

    Update treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,10 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",  -- Use stable branch, not the rewrite
+    branch = "main",
     build = ":TSUpdate",
     config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
+      require("nvim-treesitter").setup()
     end
   }
 }
